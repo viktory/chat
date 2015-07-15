b923b7eab4daf30694c7f317307a434ff93bff84
Viktory <lysenkoviktory@gmail.com>
2015-07-16 00:08:19 +0300

    empty users and dialogs case

--- resources/views/chat/_dialogs.blade.php
+++ resources/views/chat/_dialogs.blade.php
@@ -1,8 +1,10 @@
 <div class="list-group">
-    @foreach ($dialogs as $dialog)
+    @forelse ($dialogs as $dialog)
         <a href="{{ route('load-history', ['from' => $dialog->from, 'to' => $dialog->to]) }}" class="list-group-item" data-from="{{ $dialog->from }}" data-to="{{ $dialog->to }}">
             {{ $dialog->sender->username }} &amp; {{ $dialog->addressee->username }}
             <span class="counter"></span>
         </a>
-    @endforeach
+    @empty
+        <div class="alert alert-warning">Unfortunately, users don't want to chat(</div>
+    @endforelse
 </div>
--- resources/views/chat/_users.blade.php
+++ resources/views/chat/_users.blade.php
@@ -1,8 +1,10 @@
 <div class="list-group">
-    @foreach ($users as $user)
+    @forelse ($users as $user)
         <a href="{{ route('load-history', ['from' => $user->id]) }}" class="list-group-item" data-id="{{ $user->id }}">
             {{ $user->username }}
             <span class="counter"></span>
         </a>
-    @endforeach
+    @empty
+        <div class="alert alert-warning">You are the only user</div>
+    @endforelse
 </div>
